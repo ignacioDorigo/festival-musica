9d49a6eae7dd055bd10cf738b3efc681c16bccbc
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-04-18 21:40:35 -0300

    header, video y festival

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
-// import 
+import { src, dest, watch, series } from "gulp";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
 
-export function css(done) {}
+const sass = gulpSass(dartSass);
+
+export function css(done) {
+  src("src/scss/**/*.scss").pipe(sass()).pipe(dest("build/css"));
+  done();
+}
+
+export function watchFiles() {
+  watch("src/scss/**/*.scss", css);
+}
+
+export default series(css, watchFiles);
